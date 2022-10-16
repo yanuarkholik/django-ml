bec446e6945183f8fc272262d18a91e74196a6f1
yanuarkholik <yanuar.0974@students.amikom.ac.id>
2022-10-16 17:00:36 +0700

    test case and db

--- learning/settings.py
+++ learning/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'widget_tweaks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
